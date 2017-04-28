6db58de11ef45e91e7e756a6f4d946d722bb2ada
TanLe <tan.le@beesightsoft.com>
2017-04-28 16:44:50 +0700

    Return absolute path in IOS

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -23,33 +23,30 @@
 {
     self.callbackId = command.callbackId;
 	NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
-}
 
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-{
-    // Was there an error?
-    if (error != NULL)
-    {
-        // Show error message...
-        NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    }
-    else  // No errors
-    {
-        // Show message image successfully saved
-        NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    }
-}
+	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
+  ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
 
+  [library writeImageToSavedPhotosAlbum: image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error){
+  		if (error)
+  		{
+  			// Show error message...
+  			NSLog(@"ERROR: %@",error);
+  			CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+  			[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+  		}
+  		else
+  		{
+  			// Show message image successfully saved
+  			NSLog(@"Saved URL : %@", assetURL);
+  			CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:[assetURL absoluteString]];
+  			[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+  		}
+  }];
+  [library release];
+}
 - (void)dealloc
-{	
+{
 	[callbackId release];
     [super dealloc];
 }
